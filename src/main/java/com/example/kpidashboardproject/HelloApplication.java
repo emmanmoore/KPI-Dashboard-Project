f3d94db184457800ca822e08384d7154d71ec333
AleksJay <116003726+AleksJay@users.noreply.github.com>
2022-11-22 16:33:24 -0500

    Update HelloApplication.java
    changed width and height

--- src/main/java/com/example/kpidashboardproject/HelloApplication.java
+++ src/main/java/com/example/kpidashboardproject/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("KPIDashboard.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1327, 741);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
