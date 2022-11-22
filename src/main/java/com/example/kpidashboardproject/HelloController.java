ffbe3376934c3433281f36349321fea33ce7a6c5
AleksJay <116003726+AleksJay@users.noreply.github.com>
2022-11-22 16:26:03 -0500

    Update HelloController.java
    this is random code with ideas sprinkled in
    
    I commented all fx:ids
    added some arrays, but this will need to be changed (we may consider making these observablelists)
    I added example/sample code, because we will need to convert all array information into series so that the line graphs can display it

--- src/main/java/com/example/kpidashboardproject/HelloController.java
+++ src/main/java/com/example/kpidashboardproject/HelloController.java
@@ -1,9 +1,65 @@
 package com.example.kpidashboardproject;
 
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+//    final NumberAxis xAxis = new NumberAxis();
+//    final NumberAxis yAxis = new NumberAxis();
+//
+//    LineChart<Number,Number lineChart = new LineChart<Number,Number>(xAxis,yAxis);
+//
+//    XYChart.Series series = new XYChart.Series();
+//    series.setName("data");
+//    for(int i = 0; i<xArray.length; i++) {
+//        series.getData().add(new XYChart.Data(xArray[i], yArray[i]);
+//    }
+//    lineChart.getData().add(series);
+
+    //fx:ids for JAVAFX file:
+    //appointment-listing conversion rate: ALCRLineChart
+    //average commission per sale: ACPSLineChart
+    //average square foot: ASFLineChartY
+    //average price per square foot: ASFPriceLineChart
+    //listings closed: LCLineChart
+    //average time on market: ATMLineChart
+    //percentage of homes sold from inventory: HomesSoldLineChart
+    //toggle view button: ToggleViewButton
+    //KPI in title "kpi dashboard": KPITitleText
+    //dashboard in title "kpi dashboard": DashboardTitleText
+    //12 month real estate analysis title: RealEstateTitleText
+    //background rectangle for average commission per sale: ACPSRectangle
+
+    private static final NumberFormat currency = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percent = NumberFormat.getPercentInstance();
+
+    private BigDecimal Percentage = new BigDecimal(0.e15);
+
+    int[] MonthNum = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    //will correlate with numberaxis for each graph to show data over course of 12 months
+
+    double[] ConversionRate = {0.8, 0.9, 1.4, 0.8, 1.1, 0.7, 0.6, 0.3, 0.8, 0.6, 1.2, 0.9};
+
+    int[] CommissionPerSale = {16703, 21086, 20506, 20204, 24003, 25690, 14380, 17840, 15469, 14897,
+    20870, 21304};
+
+    int[] SoldHomesPerInventory = {37, 24,27, 33, 34, 29, 21, 26, 31, 34, 28, 33};
+
+    int[] listingsClosed = {196, 76, 97, 128, 135, 112, 86, 93, 81, 73, 94, 108};
+
+
+
+
     @FXML
     private Label welcomeText;
 
@@ -12,3 +68,120 @@ public class HelloController {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
+
+
+
+//comments
+//import java.util.ArrayList;
+//        import java.util.Calendar;
+//        import java.util.Collection;
+//
+//        import javafx.application.Application;
+//        import javafx.collections.FXCollections;
+//        import javafx.collections.ObservableList;
+//        import javafx.scene.Scene;
+//        import javafx.scene.chart.LineChart;
+//        import javafx.scene.chart.NumberAxis;
+//        import javafx.scene.chart.XYChart;
+//        import javafx.stage.Stage;
+//
+//public class GraphingImplementation  extends Application
+//{
+//    @Override
+//    public void start(Stage stage)
+//    {
+//        // close the initial stage.
+//        stage.close();
+//
+//        // The size of the array
+//        int seriesSize = 50000;
+//
+//        // Build the graphs
+//        LineChart<Number, Number> graph1 = new LineChart<Number, Number>(new NumberAxis(), new NumberAxis());
+//        LineChart<Number, Number> graph2 = new LineChart<Number, Number>(new NumberAxis(), new NumberAxis());
+//
+//        // Deactivate features that cause slow down
+//        graph1.setCreateSymbols(false);
+//        graph1.setAnimated(false);
+//        graph2.setCreateSymbols(false);
+//        graph2.setAnimated(false);
+//
+//        // Create value array to graph.
+//        int value[] = new int[seriesSize];
+//        for(int i = 0; i<seriesSize; i++)
+//            value[i] = i;
+//
+//
+//
+//        //**********************************************************
+//        // METHOD #1 - Creation of collection to populate series
+//        //**********************************************************
+//        // TIME COMPLEXITY - O{n}
+//        //**********************************************************
+//        // Mark the starting time of graphing method #1
+//        long startTime = Calendar.getInstance().getTimeInMillis();
+//        // Create the collection
+//        Collection<XYChart.Data<Number, Number>> collection = new ArrayList<XYChart.Data<Number, Number>>();
+//        // Add all values into the collection
+//        for (int val : value)
+//            collection.add(new XYChart.Data<Number, Number>(val,val));
+//        // Create the data using the collection
+//        ObservableList<XYChart.Data<Number, Number>> list = FXCollections.observableArrayList(collection);
+//        // create a series from the list
+//        XYChart.Series<Number, Number> series = new XYChart.Series<Number, Number>(list);
+//        // Add the series to the graph
+//        graph1.getData().add(series);
+//        // Mark the stopping time of graphing method #1
+//        long stopTime = Calendar.getInstance().getTimeInMillis();
+//        // Print the run time for method #1
+//        System.out.println((stopTime - startTime) + " Milliseconds");
+//
+//
+//
+//        // build the first scene
+//        Scene scene1 = new Scene(graph1, 640, 480);
+//        // create and populate the first stage
+//        Stage stage1 = new Stage();
+//        stage1.setScene(scene1);
+//        // show the first stage
+//        stage1.show();
+//
+//
+//
+//
+//        //**********************************************************
+//        // METHOD #2 - Direct creation of a series
+//        //**********************************************************
+//        // TIME COMPLEXITY - O{n^2}
+//        //**********************************************************
+//        // Mark the starting time of graphing method #2
+//        startTime = Calendar.getInstance().getTimeInMillis();
+//        // Create the XYChart Series
+//        XYChart.Series<Number, Number> series2 = new XYChart.Series<Number, Number>();
+//        // add all values to the series
+//        for (long val : value)
+//            series2.getData().add(new XYChart.Data<Number, Number>(val, val));
+//        // add the series to the graph
+//        graph2.getData().add(series2);
+//        // Mark the stopping time of graphing method #2
+//        stopTime = Calendar.getInstance().getTimeInMillis();
+//        // Print the run time for method #2
+//        System.out.println((stopTime - startTime) + " Milliseconds");
+//
+//
+//
+//        // build the second scene
+//        Scene scene2 = new Scene(graph2, 640, 480);
+//        // create and populate the second stage
+//        Stage stage2 = new Stage();
+//        stage2.setScene(scene2);
+//        // show the second stage
+//        stage2.show();
+//    }
+//
+//    public static void main(String[] args)
+//    {
+//        launch(args);
+//    }
+//
+//}
