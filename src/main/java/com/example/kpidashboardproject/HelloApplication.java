cbae6fc3109dfee0c7811ec369aa16509ca94d83
lskellerm <lekellermann95@gmail.com>
2022-11-26 16:18:53 -0500

    Created root Scene Graph node and attached Scene Graph to the Scene Graph

--- src/main/java/com/example/kpidashboardproject/HelloApplication.java
+++ src/main/java/com/example/kpidashboardproject/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.kpidashboardproject;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,14 +11,18 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("KPIDashboard.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1327, 741);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        Parent root =
+                FXMLLoader.load(getClass().getResource("KPIDashboard.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("KPIDashboard.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 1327, 741);
+
+        Scene scene = new Scene(root); // Attach Scene graph to the new Scene whose root node is specified above
+        stage.setScene(scene); // Attach the scene to the Stage (Application Window)
+        stage.setTitle("KPI Dashboard Application!"); // Set the Title to be displayed in the Window's title bar
+        stage.show(); // Display the 
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
